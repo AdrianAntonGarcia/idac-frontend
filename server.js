20e04e09b1c75c56e351557688733d284632118b
Adrian Anton <adrtler@gmail.com>
2020-04-26 12:40:41 +0200

    Ajuste build prod

--- server.js
+++ server.js
@@ -5,5 +5,4 @@ app.use(express.static(__dirname+'/dist/idac-frontend'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/idac-frontend/index.html'));
 });
-
 app.listen(process.env.PORT || 8080);
