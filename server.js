b894d50b316ac3ca8533518b1971ff79524826be
Adrian Anton <adrtler@gmail.com>
2020-04-26 13:00:43 +0200

    Arreglo rutas angular con heroku

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-var path = require('path');
+var path = require("path");
 var express = require("express");
 var app = express();
-app.use(express.static(__dirname+'/dist/idac-frontend'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/idac-frontend/index.html'));
+// const forceSSL = function () {
+//     return function (req, res, next) {
+//         if (req.headers["x-forwarded-proto"] !== "https") {
+//             return res.redirect(
+//                 ["https://", req.get("Host"), req.url].join("")
+//             );
+//         }
+//         next();
+//     };
+// };
+// app.use(forceSSL());
+app.use(express.static(__dirname + "/dist/idac-frontend"));
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname + "/dist/idac-frontend/index.html"));
 });
 app.listen(process.env.PORT || 8080);
